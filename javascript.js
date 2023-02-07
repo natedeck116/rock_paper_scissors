e5ddae4a6c30e390c5c7075ad2e267e837bf748d
Nathan Deck <nathan.deck@cc-ea.org>
2023-02-07 15:55:08 -0800

    Expand previous function
    Added an if statement to the function that after randomly selecting a
    number between 1 and 3 then either outputs 'rock' if 1, 'paper' if 2
    or 'scissors' if three

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
-// 1. Write a function that randomly selects 'Rock', 'Paper', or 'Scissors' 
+// 1. Write a function that randomly selects between 1, 2, and 3
 function getComputerChoice(min, max) {
-    //Need a function that picks between 1,2, and 3
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    
+    Math.floor(Math.random() * (max - min + 1) + min);
+// 2. Write a function that takes that number and returns either 'rock', 'paper', or 'scissors'
+    if (Math.floor(Math.random() * (max - min + 1) + min) == 1) {
+        return 'rock';
+    } else if (Math.floor(Math.random() * (max - min + 1) + min) == 2) {
+        return 'paper';
+    } else (Math.floor(Math.random() * (max - min + 1) + min) == 3) 
+        return 'scissors';
 }
 console.log( getComputerChoice(1, 3) );
-// 2. Write another function that plays only one round of rock paper scissors
-// 3. Make sure the function for one round of the game picks a winner that also
+
+// 3. Write another function that plays only one round of rock paper scissors
+// 4. Make sure the function for one round of the game picks a winner that also
 // describes what beats what (ex: paper beats rock) This also should be case insensitive for Rock RoCK and so on work.
